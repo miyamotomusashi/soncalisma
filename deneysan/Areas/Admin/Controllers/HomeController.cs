8f72c6e6fadd1cf64d2b3c842c99c570028f04af
cenbek <cenbek03@hotmail.com>
2013-05-13 21:12:20 +0300

    11

--- deneysan/Areas/Admin/Controllers/HomeController.cs
+++ deneysan/Areas/Admin/Controllers/HomeController.cs
@@ -17,148 +17,76 @@ namespace deneysan.Areas.Admin.Controllers
 
         public ActionResult Index()
         {
-             var list = TeklifManager.GetList();
-             if (RouteData.Values["type"] != null)
-             {
-                 ViewBag.Param = "0";
-                 string type = RouteData.Values["type"].ToString();
-                 //if (type == "tumteklifler")
-                 //{
-                 //    ViewBag.Header = "TÜM TEKLİFLER";
-                 //    list = TeklifManager.GetList();
-                 //}
-                 if (type == "onaybekleyenler")
-                 {
-                     ViewBag.Header = "YENİ GELEN TEKLİFLER / ONAY BEKLEYEN TEKLİFLER";
-                     list = TeklifManager.GetList(Convert.ToInt32(EnumTeklifTip.Onaylanmadi));
-                 }
-                 else if (type == "onaylananlar")
-                 {
-                     ViewBag.Header = "ONAYLANAN TEKLİFLER";
-                     list = TeklifManager.GetList(Convert.ToInt32(EnumTeklifTip.Onaylandi));
-                 }
-                 else if (type == "iptaledilenler")
-                 {
-                     ViewBag.Header = "İPTAL EDİLEN TEKLİFLER";
-                     list = TeklifManager.GetList(Convert.ToInt32(EnumTeklifTip.Iptal));
-                 }
+            return View();
+             //var list = TeklifManager.GetList();
+             //if (RouteData.Values["type"] != null)
+             //{
+             //    ViewBag.Param = "0";
+             //    string type = RouteData.Values["type"].ToString();
+             //    //if (type == "tumteklifler")
+             //    //{
+             //    //    ViewBag.Header = "TÜM TEKLİFLER";
+             //    //    list = TeklifManager.GetList();
+             //    //}
+             //    if (type == "onaybekleyenler")
+             //    {
+             //        ViewBag.Header = "YENİ GELEN TEKLİFLER / ONAY BEKLEYEN TEKLİFLER";
+             //        list = TeklifManager.GetList(Convert.ToInt32(EnumTeklifTip.Onaylanmadi));
+             //    }
+             //    else if (type == "onaylananlar")
+             //    {
+             //        ViewBag.Header = "ONAYLANAN TEKLİFLER";
+             //        list = TeklifManager.GetList(Convert.ToInt32(EnumTeklifTip.Onaylandi));
+             //    }
+             //    else if (type == "iptaledilenler")
+             //    {
+             //        ViewBag.Header = "İPTAL EDİLEN TEKLİFLER";
+             //        list = TeklifManager.GetList(Convert.ToInt32(EnumTeklifTip.Iptal));
+             //    }
 
-                 return View(list);
-             }
-             else
-             {
-                 ViewBag.Header = "YENİ GELEN TEKLİFLER / ONAY BEKLEYEN TEKLİFLER";
-                 list = TeklifManager.GetList(Convert.ToInt32(EnumTeklifTip.Onaylanmadi));
-                 return View(list);
-             }
+             //    return View(list);
+             //}
+             //else
+             //{
+             //    ViewBag.Header = "YENİ GELEN TEKLİFLER / ONAY BEKLEYEN TEKLİFLER";
+             //    list = TeklifManager.GetList(Convert.ToInt32(EnumTeklifTip.Onaylanmadi));
+             //    return View(list);
+             //}
         }
 
-        public ActionResult AllList()
-        {
-            var list = TeklifManager.GetList();
-            return View(list);
-        }
+        //public ActionResult AllList()
+        //{
+        //    var list = TeklifManager.GetList();
+        //    return View(list);
+        //}
 
 
-        public ActionResult Details(int id)
-        {
-            int teklifid = 0;
-            if (RouteData.Values["id"] != null)
-            {
-                bool isnumber = int.TryParse(RouteData.Values["id"].ToString(), out teklifid);
-                if (isnumber)
-                {
-                    var teklif = TeklifManager.GetTeklifById(teklifid);
-                    var urunler = TeklifManager.GetUrunList(teklifid);
-                    TeklifModel modelbind = new TeklifModel(teklif, urunler);
-                    return View(modelbind);
-                }
-                else
-                    return View();
+        //public ActionResult Details(int id)
+        //{
+        //    int teklifid = 0;
+        //    if (RouteData.Values["id"] != null)
+        //    {
+        //        bool isnumber = int.TryParse(RouteData.Values["id"].ToString(), out teklifid);
+        //        if (isnumber)
+        //        {
+        //            var teklif = TeklifManager.GetTeklifById(teklifid);
+        //            var urunler = TeklifManager.GetUrunList(teklifid);
+        //            TeklifModel modelbind = new TeklifModel(teklif, urunler);
+        //            return View(modelbind);
+        //        }
+        //        else
+        //            return View();
            
-            }
-            else
-                return View();
+        //    }
+        //    else
+        //        return View();
            
            
-        }
+        //}
 
         //
         // GET: /Admin/Home/Create
 
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Admin/Home/Create
-
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /Admin/Home/Edit/5
-
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /Admin/Home/Edit/5
-
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /Admin/Home/Delete/5
-
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /Admin/Home/Delete/5
-
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+      
     }
 }
