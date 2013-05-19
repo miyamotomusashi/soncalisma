3bfe818bd79d423d594a5784c3a3a754d82e0631
cenbek <cenbek03@hotmail.com>
2013-05-19 14:20:59 +0300

    proj dosyasi

---
+++ deneysan_Data/Entities/MailSetting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deneysan_Data.Entities
+{
+    public class MailSetting
+    {
+        [Key]
+        public int MailSettingId { get; set; }
+        [Display(Name = "Server Mail Adresi")]
+        [Required(ErrorMessage = "Server Mail Adresini Giriniz.")]
+        [EmailAddress(ErrorMessage = "Mail Adresi Doğru Formatta Değil.")]
+        public string ServerMail{ get; set; }
+       
+        [Display(Name = "SMTP Host Adresi")]
+        [Required(ErrorMessage = "Host Adresini Giriniz.")]
+        public string ServerHost { get; set; }
+        
+        [Display(Name = "Port Numarası")]
+        [Required(ErrorMessage = "Port Numarasını Giriniz.")]
+        public int Port { get; set; }
+       
+        [Display(Name = "Şifre")]
+        public string Password { get; set; }
+    }
+}
