0f3a2aceff459fd035ad69964c7cf51244c23349
cenbek <cenbek03@hotmail.com>
2013-05-21 21:45:56 +0300

    teklif detay duzenleemeleri

--- deneysan/Areas/Admin/Content/scripts/pagescripts/teklif/teklif.js
+++ deneysan/Areas/Admin/Content/scripts/pagescripts/teklif/teklif.js
@@ -7,3 +7,131 @@
         MessageBox("İşlem Sırasında Bir Hata Oluştu.", "alert");
 
 });
+
+function EditRecord(id) {
+
+    $("#spantextfiyat_" + id).css("display", "none");
+    $("#textfiyat_" + id).css("display", "block");
+    $("#textfiyat_" + id).val($("#spantextfiyat_" + id).text());
+
+    $("#spantextdonanim_" + id).css("display", "none");
+    $("#textdonanim_" + id).css("display", "block");
+    $("#textdonanim_" + id).val($("#spantextdonanim_" + id).text());
+
+    $("#spantextadet_" + id).css("display", "none");
+    $("#textadet_" + id).css("display", "block");
+    $("#textadet_" + id).val($("#spantextadet_" + id).text());
+
+   
+    $("#btn_update_" + id).css("display", "none");
+    $("#btn_save_" + id).attr("src", "/Areas/Admin/Content/images/icons/16/save.png");
+    $("#btn_save_" + id).css("display", "inline-block");
+    $("#btn_cancel_" + id).css("display", "inline-block");
+    $("#DeleteImage_" + id).css("display", "none");
+    $("#textitem_" + id).focus();
+}
+
+function CancelRecord(id) {
+    $("#spantextfiyat_" + id).css("display", "block");
+    $("#textfiyat_" + id).css("display", "none");
+  
+    $("#spantextdonanim_" + id).css("display", "block");
+    $("#textdonanim_" + id).css("display", "none");
+ 
+    $("#spantextadet_" + id).css("display", "block");
+    $("#textadet_" + id).css("display", "none");
+  
+    $("#btn_update_" + id).css("display", "inline-block");
+    $("#btn_save_" + id).css("display", "none");
+    $("#btn_cancel_" + id).css("display", "none");
+    $("#DeleteImage_" + id).css("display", "inline-block");
+
+}
+
+function UpdateRecord(id) {
+    //$("#spantextfiyat_" + id).css("display", "none");
+    //$("#textfiyat_" + id).css("display", "block");
+    //$("#textfiyat_" + id).val($("#spantextfiyat_" + id).text());
+
+    //$("#spantextdonanim_" + id).css("display", "none");
+    //$("#textdonanim_" + id).css("display", "block");
+    //$("#textdonanim_" + id).val($("#spantextdonanim_" + id).text());
+
+    //$("#spantextadet_" + id).css("display", "none");
+    //$("#textadet_" + id).css("display", "block");
+    //$("#textadet_" + id).val($("#spantextadet_" + id).text());
+
+
+    var fiyat = $("#textfiyat_" + id).val();
+    if (fiyat == "") {
+        $.msgbox("Fiyat Alanına Bir Değer Giriniz!", {
+            type: "alert", buttons: [{ type: "submit", value: "Tamam" }]
+        });
+        return;
+    }
+
+    var fiyatclear = ReplaceAll(fiyat, "TL", "");
+
+    var donanim = $("#textdonanim_" + id).val();
+    if (donanim == "") {
+        $.msgbox("Donanım Fiyatı Alanına Bir Değer Giriniz!", {
+            type: "alert", buttons: [{ type: "submit", value: "Tamam" }]
+        });
+        return;
+    }
+
+    var donanimclear = ReplaceAll(donanim, "TL", "");
+
+    var adet = $("#textadet_" + id).val();
+    if (adet == "") {
+        $.msgbox("Adet Alanına Bir Değer Giriniz!", {
+            type: "alert", buttons: [{ type: "submit", value: "Tamam" }]
+        });
+        return;
+    }
+
+
+    
+    alert(donanim);
+    return;
+    var cssbackgrnd = $("#listItem_" + id + " td").css("background-color");
+    $("#btn_save_" + id).attr("src", "/Areas/Admin/Content/images/icons/16/loader.gif");
+
+    $.ajax({
+        type: 'POST',
+        url: '/Teklif/UpdateRecord',
+        data: '{id:"' + id + '",name:"' + name + '"}',
+        contentType: "application/json; charset=utf-8",
+        dataType: 'json',
+        success: function () {
+
+            $("#btn_save_" + id).css("display", "none");
+            $("#btn_update_" + id).css("display", "inline-block");
+            $("#btn_cancel_" + id).css("display", "none");
+            $("#DeleteImage_" + id).css("display", "inline-block");
+
+            $("#spanitem_" + id).css("display", "block");
+            $("#spanitem_" + id).text($("#textitem_" + id).val());
+            $("#textitem_" + id).css("display", "none");
+
+            var odd = $("#listItem_" + id).attr("class");
+
+            if (odd == "odd")
+                $("#listItem_" + id + " td").animate({ backgroundColor: "#66ff66" }, 'slow').delay(500).animate({ backgroundColor: "#F2F2F2" }, 'slow');
+            else
+                $("#listItem_" + id + " td").animate({ backgroundColor: "#66ff66" }, 'slow').delay(500).animate({ backgroundColor: "#fff" }, 'slow');
+
+        },
+        error: function () {
+            $.msgbox("İşlem Sırasında Bir Hata Oluştu.", {
+                type: "alert", buttons: [{ type: "submit", value: "Tamam" }]
+            });
+
+        }
+
+
+
+    });
+
+}
+
