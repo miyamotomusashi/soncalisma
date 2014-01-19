4f90c3c522d38deac17eaf2f4cb6694e62bb9ac7
sakurahoshi <cetintozkoparan@hotmail.com>
2014-01-19 17:13:24 +0200

    yeni teklif eklendi

---
+++ deneysan_Data/Entities/OfferNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deneysan_DAL.Entities
+{
+    public class OfferNumber
+    {
+        public int OfferNumberId { get; set; }
+        public int LastNumber { get; set; }
+    }
+}
